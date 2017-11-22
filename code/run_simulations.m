50e0bffeb264bd74b0eb72d9ab336bf5d4a8c118
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2017-11-22 15:00:05 +0100

    again

--- code/run_simulations.m
+++ code/run_simulations.m
@@ -42,14 +42,8 @@ end
 % 'HPMPC_BX'    hpmpc with its own partial condensing and block size X
 % 'FORCES'      FORCES QP solver (if license is available)
 
-<<<<<<< HEAD
-set_of_solvers = {'qpOASES_N3', 'FORCES', 'qpOASES_N2', 'qpDUNES_B0', 'HPMPC_B0'}; % choose solvers
-set_of_N       = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};      % choose horizon length (for all solvers)
-% set_of_N       = {10};      % choose horizon length (for all solvers)
-=======
 set_of_solvers = {'qpOASES_N2', 'HPMPC_B0'};                 % choose solvers
 set_of_N       = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};  % choose horizon length (for all solvers)
->>>>>>> 1acf4c8c8fd2fc89af9f941bcd486b6b774dc2ad
 
 sim_opts.NMASS       = 6;
 sim_opts.NRUNS       = 5;
@@ -57,11 +51,7 @@ sim_opts.MPC_EXPORT  = 1;
 sim_opts.MPC_COMPILE = 1;
 sim_opts.SIM_EXPORT  = 1;
 sim_opts.SIM_COMPILE = 1;
-<<<<<<< HEAD
-sim_opts.CHECK_AGAINST_REF_SOL = 1;
-=======
 sim_opts.CHECK_AGAINST_REF_SOL = 0;
->>>>>>> 1acf4c8c8fd2fc89af9f941bcd486b6b774dc2ad
 sim_opts.SOL_TOL = 1e-3;
 
 %% Run simulations
