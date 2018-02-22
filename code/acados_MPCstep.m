abd11ee6dc3a02dfb4a614bd011e7aafca3e8167
Dimitris Kouzoupis <dimitris.kouzoupis@imtek.uni-freiburg.de>
2018-02-22 14:29:03 +0100

    updated acados_step

--- code/acados_MPCstep.m
+++ code/acados_MPCstep.m
@@ -1,25 +1,18 @@
 function output = acados_MPCstep( input )
     import acados.*
-    
+
     persistent qp;
-    
+
+    if isempty(qp)
+        first_iter = 1;
+    else
+        first_iter = 0;
+    end
+
     solver = input.solver;
-%     if strfind(input.solver, 'qpOASES')
-%         
-%         solver = 'qpoases';
-%         
-%     elseif strfind(input.solver, 'qpDUNES')
-%         
-%         solver = 'qpdunes';
-%         
-%     elseif strfind(input.solver, 'HPMPC')
-%         
-%         solver = 'hpmpc';
-%         
-%     end
-         
+
     %% extract data from input
-    
+
     NX           =  size(input.x,2);
     NU           =  size(input.u,2);
     N            =  size(input.u,1);
@@ -34,19 +27,19 @@ function output = acados_MPCstep( input )
     lbu          =  input.LBU;
     ubu          =  input.UBU;
     WALL         =  input.WALL;
-   
+
     lbx          =  -inf*ones(NX,1);
     ubx          =  inf*ones(NX,1);
     lbx(2:3:3*(M+1)) = WALL;
-    
+
     %% evaluate sensitivities
-    
+
     SensX = cell(N,1);
     SensU = cell(N,1);
     SensC = cell(N,1);
     GradX = cell(N+1,1);
     GradU = cell(N,1);
- 
+
     INT_NAME = sprintf('integrate_chain_M%d',M+2);
     for ii = 1:N
         sim_input.x = state_traj(:,ii);
@@ -55,7 +48,7 @@ function output = acados_MPCstep( input )
         if out.errorCode
             error('integrator is failed')
         end
-        SensX{ii,1} = states.sensX; 
+        SensX{ii,1} = states.sensX;
         SensU{ii,1} = states.sensU;
         SensC{ii,1} = states.value - state_traj(:,ii+1);
         GradX{ii,1} = Q*(sim_input.x-X_ref(:,ii));
@@ -65,20 +58,33 @@ function output = acados_MPCstep( input )
 
 
     %% solve with acados
-    
-    if isempty(qp)
-        warning('first iteration')
+
+    if first_iter
+        % warning('temp bypassing first iter')
         qp = ocp_qp(N, NX, NU);
-   
+
         qp.set('Q', Q);
         qp.set('R', R);
         qp.set('Q', N, QN);
-        
-        opts = struct('max_iter', 1000, 'warm_start', input.warmstart);
-        
-        if strcmp(solver, 'qpdunes') || strcmp(solver, 'hpmpc')
-            if ~isinf(input.N2)
+
+        % max iter
+        opts = struct('max_iter', 1000);
+
+        % warmstart (for qpOASES, QORE and qpDUNES)
+        if ~contains(solver, 'hpipm') && ~contains(solver, 'hpmpc')
+            opts.warm_start = input.warmstart;
+            if strcmp(solver, 'qore')
+                %opts.warm_strategy = 0;  % 0 or 1
+            end
+        end
+
+        % partial condensing
+        if ~strcmp(solver, 'qpoases') && ~strcmp(solver, 'qore')&& ~strcmp(solver, 'condensing_hpipm')
+            if ~isinf(input.N2) && input.N2 ~= N
                 opts.N2 = input.N2;
+                if strcmp(solver, 'qpdunes')
+                    opts.clipping = 0;
+                end
             else
                 opts.N2 = N;
                 if strcmp(solver, 'qpdunes')
@@ -86,25 +92,26 @@ function output = acados_MPCstep( input )
                 end
             end
         end
-        
-        % TODO: FIX SEGFAULTS IN HPMPC!!
+
+        % TODO: fix segfaults in HPMPC!!!
         if strcmp(solver, 'hpmpc')
             opts.mu0 = 1e6;
-            % opts.tol = 1e-12; % TODO: WHY SETTING THIS VIA OPTS BREAKS SOLVER?
+            % TODO: Why setting this (same as default arg) breaks solver?
+            % opts.tol = 1e-12;
         end
-        
+        opts
         qp.initialize_solver(solver, opts);
     end
-    
+
     for ii = 0:N-1
         qp.set('A', ii, SensX{ii+1});
         qp.set('B', ii, SensU{ii+1});
-        qp.set('b', ii, SensC{ii+1}); 
+        qp.set('b', ii, SensC{ii+1});
         qp.set('q', ii, GradX{ii+1});
-        qp.set('r', ii, GradU{ii+1});        
+        qp.set('r', ii, GradU{ii+1});
     end
     qp.set('q', N, GradX{N+1});
-    
+
     % specify initial condition
     for ii = 0:N-1
         qp.set('lbu', ii, lbu - control_traj(:,ii+1));
@@ -114,15 +121,21 @@ function output = acados_MPCstep( input )
     end
     qp.set('lbx', 0, input.x0' - state_traj(:,1));
     qp.set('ubx', 0, input.x0' - state_traj(:,1));
-    
+
+    if first_iter
+        qp.initialize_solver(solver, opts);
+    else
+       keyboard
+    end
+
     % solve QP
     acados_output = qp.solve();
-    
+
     Delta_xopt = acados_output.states();
     Delta_uopt = acados_output.controls();
 
     %% write output
-    
+
     output.x   = (state_traj   + [Delta_xopt{:}])';
     output.u   = (control_traj + [Delta_uopt{:}])';
     output.info.QP_time = acados_output.info.total_time;
